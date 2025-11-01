3056d6eb7fe2c5ec9341bedc79de9ae7dda3543a
Jordy Loor <jloorm2003@gmail.com>
2025-11-01 17:44:59 -0500

    [UPDATE] Relacion Pago-Reserva para mostrar el historial de pagos

--- app/pagos/views.py
+++ app/pagos/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, filters
 from .models import Pago
 from .serializers import PagoSerializer
 from drf_spectacular.utils import extend_schema
+from app.reservas.models import Reserva
 
 @extend_schema(tags=['Pagos'])
 class PagoViewSet(viewsets.ModelViewSet):
@@ -13,3 +14,16 @@ class PagoViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['tipo_pago', 'estado']
     filterset_fields = ['tipo_pago', 'estado']
+
+    def perform_create(self, serializer):
+        # Guarda el pago primero
+        pago = serializer.save()
+        # Verifica si el front envió una reserva_id
+        reserva_id = self.request.data.get('reserva_id')
+        if reserva_id:
+            try:
+                reserva = Reserva.objects.get(id=reserva_id)
+                reserva.pago = pago
+                reserva.save()
+            except Reserva.DoesNotExist:
+                pass
